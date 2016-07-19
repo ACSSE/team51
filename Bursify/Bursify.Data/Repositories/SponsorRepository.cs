1e35cf096b02a362c179210b673674654a1d538a
brandon7596 <brandonsibbs@gmail.com>
2016-07-19 12:22:10 +0200

    Created new view models for data mapping.

---
+++ Bursify/Bursify.Data/Repositories/SponsorRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Bursify.Data.EF.Repositories;
+using Bursify.Entities.SponsorEntities;
+
+namespace Bursify.Data.Repositories
+{
+    public class SponsorRepository : Repository<Sponsor>
+    {
+        public SponsorRepository(DataSession dataSession) : base(dataSession)
+        {
+        }
+
+        public List<Sponsor> GetTopTenSponsors()
+        {
+            var many = this.FindMany(x => x.BursifyRank == 1 && x.BursifyUser.Email.StartsWith("blah"));
+
+            var topSponsors = ((from sponsor in DbContext.Set<Sponsor>()
+                                orderby sponsor.BursifyRank
+                                select sponsor).Distinct().Take(10)).ToList();
+
+            return topSponsors;
+        }
+
+    }
+}
---
+++ Bursify/Bursify.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Bursify.Data.EF.Repositories;
+using Bursify.Entities.CampaignEntities;
+using Bursify.Entities.StudentEntities;
+
+namespace Bursify.Data.Repositories
+{
+    public class StudentRepository : Repository<Student>
+    {
+        public StudentRepository(DataSession dataSession) : base(dataSession)
+        {
+        }
+
+        public Student GetAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Campaign GetSIngleCampaign(int id)
+        {
+            var campaign = _context.CampaignSet
+                .Include(x => x.Account).FirstOrDefault(x => x.CampaignId == id);
+
+            return campaign;
+        }
+
+        public List<Campaign> GetAllCampaigns()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
