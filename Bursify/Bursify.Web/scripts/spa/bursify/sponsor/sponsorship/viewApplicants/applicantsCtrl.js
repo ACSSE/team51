07f561f6f28a0f48f1fb425c1681b9464d2ed06d
Mackis <malcolm@mdevsa.com>
2016-08-24 18:19:53 +0200

    - added view for sponsor to view applicants

---
+++ Bursify/Bursify.Web/scripts/spa/bursify/sponsor/sponsorship/viewApplicants/applicantsCtrl.js
@@ -0,0 +1,18 @@
+﻿(function (app) {
+    'use strict';
+
+    app.controller('applicantsCtrl', applicantsCtrl);
+
+    applicantsCtrl.$inject = ['$scope', '$rootScope', 'apiService', 'notificationService', '$routeParams'];
+
+    function applicantsCtrl($scope, $rootScope, apiService, notificationService, $routeParams) {
+        $scope.pageClass = 'page-view-applicants';
+
+        apiService.get('/api/sponsorship/GetApplicants/?sponsorshipId=' + $routeParams.sponsorshipId, null, applicantsLoadCompleted, applicantsLoadFailed);
+
+
+
+    }
+
+})(angular.module('BursifyApp'));
+
