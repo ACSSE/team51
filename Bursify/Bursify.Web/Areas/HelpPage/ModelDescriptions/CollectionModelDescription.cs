df26ac9c6c7b08386d9a379f17e955f97ab6517b
brandon7596 <brandonsibbs@gmail.com>
2016-10-24 12:46:37 +0200

    Added help pages for web api.

---
+++ Bursify/Bursify.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Bursify.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
